b2871aa460804b521ad41a01ec9103f8970df718
megasimpxd <92126746+megasimpxd@users.noreply.github.com>
2024-07-05 01:49:22 -0500

    add loading

--- api.facebook.com.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var localFilePath = 'main/html/index.html';
-
-    var iframe = document.createElement('iframe');
-    iframe.src = localFilePath;
-    iframe.style.cssText = 'width: 100%; height: 100vh; border: none; position: absolute; top: 0; left: 0;';
-    document.body.appendChild(iframe);
-});
